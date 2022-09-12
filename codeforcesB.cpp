7d0eb7bf7dc21c8b7c202bd2223ce9702700f021
parikhasharma <sharmaparikha@gmail.com>
2022-09-12 23:25:16 +0530

    educational round 135 div 2 ques B

---
+++ codeforcesB.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+       int n;
+       cin>>n;
+        vector<int> p(n);
+        iota(p.begin(), p.end(), 1);
+        for (int i = n & 1; i < n - 2; i += 2) swap(p[i], p[i + 1]);
+        for (int &x : p) cout << x << ' ';
+        cout << '\n';
+        t--;
+    }
+}
